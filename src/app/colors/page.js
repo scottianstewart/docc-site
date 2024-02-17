6971b24fceaceefa4ec87d384991132029a5fb19
Scott Stewart <scott@scottstewart.co>
2024-02-17 15:09:09 -0700

    Edit colors page, add hexes

--- src/app/colors/page.js
+++ src/app/colors/page.js
@@ -4,21 +4,23 @@ import colors from "dictionary-of-colour-combinations";
 
 export default function Home() {
   return (
-    <main className="m-4 lg:m-10">
-      <p className="font-bold mb-16">Colors</p>
-      <div className="flex flex-wrap gap-16">
+    <div>
+      <div className="flex flex-wrap mt-16 gap-16">
         {colors.map((color, i) => {
           return (
-            <div key={i} className={"flex flex-col gap-6"}>
+            <div key={i} className="flex flex-col gap-6">
               <div
                 style={{ backgroundColor: color.hex }}
                 className="h-48 w-48"
               />
-              <p className="text-sm opacity-50">{color.name}</p>
+              <div className="flex flex-col gap-1">
+                <p className="text-sm opacity-50">{color.name}</p>
+                <p className="text-sm opacity-50">{color.hex}</p>
+              </div>
             </div>
           );
         })}
       </div>
-    </main>
+    </div>
   );
 }
